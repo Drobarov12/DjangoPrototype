ee7f72d2ac4bd4e0392f4c02ed328cfc06e9fca4
Drobarov12 <drobarovmartin@yahoo.com>
2022-07-04 18:47:31 +0200

    fix issues

--- CriptoBeginners/CriptoBeginners/settings.py
+++ CriptoBeginners/CriptoBeginners/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middelware.WhiteNoiseMiddlewere',
+    
 ]
 
 ROOT_URLCONF = 'CriptoBeginners.urls'
